4c8df6072e3fe93730a6269fe9e40ebe9062178d
hamidreza <h.r.akbarnezhad@gmail.com>
2023-10-05 19:35:28 +0330

    add featured, company and name filter to products

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,25 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res, next) => {
-  const products = await Product.find();
+  const queryObject = {};
+  const { name } = req.query;
+  if (name) {
+    queryObject.name = { $regex: req.query.name, $options: 'i' };
+  }
+  const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res, next) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
   if (company) {
     queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = new RegExp(name, 'igm');
   }
   const projection = {
     name: 1,

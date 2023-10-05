593a8f96dd77e2f979eaf73dac434355e8d53710
hamidreza <h.r.akbarnezhad@gmail.com>
2023-10-05 19:06:30 +0330

    add featured & company to getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,27 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res, next) => {
-  res.status(200).json({ msg: 'get all products statics' });
+  const products = await Product.find();
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res, next) => {
-  res.json({ msg: `get all product` });
+  const { featured, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  const projection = {
+    name: 1,
+    price: 1,
+    company: 1,
+    featured: 1,
+    _id: -1,
+  };
+  const products = await Product.find(queryObject, projection);
+  res.json({ nbHits: products.length, products });
 };
 
 module.exports = {

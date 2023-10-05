e7c96c418ac2e0528180065b4a45ba664c1d2377
hamidreza <h.r.akbarnezhad@gmail.com>
2023-10-06 03:05:15 +0330

    advance filter, numeric filter, custom fields, sort, pagination added to getAllProducts

--- controllers/products.js
+++ controllers/products.js
@@ -1,16 +1,15 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res, next) => {
-  const queryObject = {};
-  const { name } = req.query;
-  if (name) {
-    queryObject.name = { $regex: req.query.name, $options: 'i' };
-  }
-  const products = await Product.find(queryObject);
-  res.status(200).json({ products, nbHits: products.length });
+  const products = await Product.find({})
+    .sort('name price')
+    .select('name price')
+    .skip(req.query.skip)
+    .limit(10);
+  res.status(200).json({ nbHits: products.length, products });
 };
 const getAllProducts = async (req, res, next) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   //#region filter
   if (featured) {
@@ -22,8 +21,48 @@ const getAllProducts = async (req, res, next) => {
   if (name) {
     queryObject.name = new RegExp(name, 'igm');
   }
+
+  //#region numericFilter
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '<': '$lt',
+      '<=': '$lte',
+      '=': '$eq',
+      '!=': '$ne',
+    };
+    const filterList = numericFilters.split(',');
+    const options = ['price', 'rating']; //just accept numeric fields
+    filterList.forEach((filter) => {
+      const [field, operator, value] = filter
+        .match(/(.*?)([<>=]+)(.*)/)
+        .slice(1);
+      if (!options.includes(field)) return;
+
+      if (!queryObject[field]) {
+        queryObject[field] = {};
+      }
+      const mongoOperator = operatorMap[operator];
+      queryObject[field][mongoOperator] = value;
+    });
+  }
+  //#endregion numericFilter
+
   //#endregion filter
+
   let result = Product.find(queryObject);
+
+  //#region fields
+  if (fields) {
+    const fieldsList = fields
+      .split(',')
+      .map((field) => field.trim())
+      .join(' ');
+    result = result.select(fieldsList);
+  }
+  //#endregion fields
+
   //#region sort
   if (sort) {
     const sortList = sort
@@ -35,15 +74,13 @@ const getAllProducts = async (req, res, next) => {
     result = result.sort('createdAt');
   }
   //#endregion sort
-  //#region fields
-  if (fields) {
-    const fieldsList = fields
-      .split(',')
-      .map((field) => field.trim())
-      .join(' ');
-    result = result.select(fieldsList);
-  }
-  //#endregion fields
+
+  //#region pagination
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 10;
+  result = result.skip((page - 1) * limit).limit(limit);
+  //#endregion pagination
+
   const products = await result;
   res.json({ nbHits: products.length, products });
 };
